a0a4f1550a43855603a2fa6f77ef3d383e354744
draialexis <drai.alexis@gmail.com>
2022-09-16 11:21:11 +0200

    :poop: Change some random string

--- Sources/Tests/Model_UTs/DieTest.cs
+++ Sources/Tests/Model_UTs/DieTest.cs
@@ -8,8 +8,8 @@ namespace Tests.Model_UTs
         [Fact]
         public void TestConstructor()
         {
-            Die die = new Die("Jerry");
-            Assert.Equal("Jerry", die.Name);
+            Die die = new Die("Ben");
+            Assert.Equal("Ben", die.Name);
         }
     }
 }
