31f988dcb07d8818eefcc2d912ddcabd0719956c
Ismail TAHA JANAN <ismailtahajanan2@gmail.com>
2022-09-17 17:42:40 +0200

    add PlayerManager class and stub class

---
+++ Sources/Model/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class PlayerManager : IManager
+    {
+        public T Add<T>(ref T toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Remove<T>(ref T toRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update<T>(ref T before, ref T after)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sources/Stub/Stub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Media;
+using System.Security.Cryptography.X509Certificates;
+using Model.Player;
+
+public class Stub
+{
+	public Stub()
+	{
+		
+	}
+
+	
+	
+}
