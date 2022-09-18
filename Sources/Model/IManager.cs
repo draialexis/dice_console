78f02448b28f7f5b411180053563e78274f87308
Alexis DRAI <drai.alexis@gmail.com>
2022-09-18 13:45:23 +0200

    :adhesive_bandage: Fix #38

--- Sources/Model/IManager.cs
+++ Sources/Model/IManager.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
 namespace Model
 {
     internal interface IManager
     {
         public T Add<T>(ref T toAdd);
-        public T Remove<T>(ref T toRemove);
+        public void Remove<T>(ref T toRemove);
         public T Update<T>(ref T before, ref T after);
+        public T GetOneById<T>(int id);
+        public IEnumerable<T> GetAll<T>();
     }
 }
