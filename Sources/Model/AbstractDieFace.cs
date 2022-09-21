20b57ce31b68f279891434af34bbe8392d4b0f81
Alexis DRAI <drai.alexis@gmail.com>
2022-09-21 13:17:08 +0200

    🚧🧑‍💻 Put Value at abstract level, leave IDs for later

--- Sources/Model/AbstractDieFace.cs
+++ Sources/Model/AbstractDieFace.cs
@@ -8,6 +8,14 @@ namespace Model
 {
     public abstract class AbstractDieFace
     {
-        public string ID { get; set; }
+        /// <summary>
+        /// every die face has a value, and they can all be represented by an int,
+        /// even if they're not litterally a decimal number
+        /// <br/>
+        /// USE GetPracticalValue for a Value specific to face type
+        /// </summary>
+        protected abstract int Value { get; }
+
+        public abstract object GetPracticalValue();
     }
 }
