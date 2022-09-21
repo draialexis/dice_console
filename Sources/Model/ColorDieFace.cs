7c0c0d5fd941ae511358a843494ce769b7617043
Alexis DRAI <drai.alexis@gmail.com>
2022-09-21 13:26:08 +0200

    ✨Use abstraction, switch from hexString to int

--- Sources/Model/ColorDieFace.cs
+++ Sources/Model/ColorDieFace.cs
@@ -8,14 +8,37 @@ namespace Model
 {
     public class ColorDieFace : AbstractDieFace
     {
-        private string ColorHex;
+        /// <summary>
+        /// a decimal representation of the hex (...representation of the color)
+        /// </summary>
+        protected override int Value { get; }
 
-        public ColorDieFace(string v)
+        /// <summary>
+        /// accepts hex strings like "ffbb84" ([RRGGBB])
+        /// </summary>
+        /// <param name="hexValueString">hex string</param>
+        public ColorDieFace(string hexValueString)
         {
-            this.ColorHex = v;
+            // https://stackoverflow.com/questions/1139957/convert-integer-to-hexadecimal-and-back-again
+
+            // if style is ("f0b"), this constructor can develop it to "ff00bb" before doing the job
+
+            Value = int.Parse(hexValueString, System.Globalization.NumberStyles.HexNumber);
         }
 
-        public string getColorHex() { return ColorHex; }
-        public void setColorHex(string ColorHex) { this.ColorHex = ColorHex; }
+        /// <summary>
+        /// accepts a decimal value that represents a color hex (0 is black, 65280 is green...)
+        /// </summary>
+        /// <param name="decimalValue"></param>
+        public ColorDieFace(int decimalValue)
+        {
+            Value = decimalValue;
+        }
+        public override object GetPracticalValue()
+        {
+            // https://stackoverflow.com/questions/1139957/convert-integer-to-hexadecimal-and-back-again
+            // maybe prepend it with a "#"...
+            return Value.ToString("X");
+        }
     }
 }
