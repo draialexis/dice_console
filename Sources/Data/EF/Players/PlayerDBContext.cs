e3687b206e5ee7028daeb9339b1cff52ae5eaa3b
Alexis DRAI <drai.alexis@gmail.com>
2022-09-30 13:05:39 +0200

    :construction: WIP on EF BP

---
+++ Sources/Data/EF/Players/PlayerDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EF.Players
+{
+    internal class PlayerDBContext : DbContext
+    {
+        public DbSet<PlayerEntity> PlayersSet { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=EFDice.DiceApp.db");
+        }
+
+    }
+}
---
+++ Sources/Data/EF/Players/PlayerDBManager.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.EF.Players
+{
+    internal class PlayerDBManager : IManager<PlayerEntity>
+    {
+        PlayerDBContext context = new PlayerDBContext();
+        
+        public PlayerEntity Add(PlayerEntity toAdd)
+        {
+            // just to check...
+
+            context.PlayersSet.Add(toAdd);
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PlayerEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerEntity GetOneByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(PlayerEntity toRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlayerEntity Update(PlayerEntity before, PlayerEntity after)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
