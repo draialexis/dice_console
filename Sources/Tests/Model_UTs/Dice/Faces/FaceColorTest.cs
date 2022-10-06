206786271ab57ae9c9eb9f7607ea2476ea305b8e
istahajana <ismail_TAHA.JANAN@rtu.uca.fr>
2022-10-06 09:58:34 +0200

    code with error

---
+++ Sources/Tests/Model_UTs/Dice/Faces/FaceColorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Model.Dice.Faces;
+using System.Drawing;
+
+namespace Tests.Model_UTs.Dice.Faces
+{
+    public class FaceColorTest
+    {
+
+
+
+        [Theory]
+        [InlineData(Color.FromName("Chocolate"))]
+        [InlineData(Color.FromArgb(144, 255, 78, 240))]
+        public void ColorFaceValueTest(Color color)
+        {
+
+
+            //Arrage 
+            ColorFace face = new ColorFace(color);
+
+
+            //Act
+            Color expected = color;
+            Color actual = face.Value;
+
+
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
