1604fece2fc380bc907161c0701a1e953997f339
Alexis DRAI <drai.alexis@gmail.com>
2022-10-06 20:40:03 +0200

    :refactor: Use Guid.NewGuid() instead of hardcoding them
    obviously ^^'

--- Sources/Data/EF/DiceAppDbContextWithStub.cs
+++ Sources/Data/EF/DiceAppDbContextWithStub.cs
@@ -17,10 +17,10 @@ namespace Data.EF
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<PlayerEntity>().HasData(
-            new PlayerEntity { ID = Guid.Parse("e3b42372-0186-484c-9b1c-01618fbfac44"), Name = "Alice" },
-            new PlayerEntity { ID = Guid.Parse("73265e15-3c43-45f8-8f5d-d02feaaf7620"), Name = "Bob" },
-            new PlayerEntity { ID = Guid.Parse("5198ba9d-44d6-4660-85f9-1843828c6f0d"), Name = "Clyde" },
-            new PlayerEntity { ID = Guid.Parse("386cec27-fd9d-4475-8093-93c8b569bf2e"), Name = "Dahlia" }
+            new PlayerEntity { ID = Guid.NewGuid(), Name = "Alice" },
+            new PlayerEntity { ID = Guid.NewGuid(), Name = "Bob" },
+            new PlayerEntity { ID = Guid.NewGuid(), Name = "Clyde" },
+            new PlayerEntity { ID = Guid.NewGuid(), Name = "Dahlia" }
             );
         }
     }
