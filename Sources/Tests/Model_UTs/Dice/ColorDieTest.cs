1ee0e762b43bdd247040cdb15ce2dac2e51324f3
Ismail TAHA JANAN <ismailtahajanan2@gmail.com>
2022-10-10 11:50:42 +0200

    :white_check_mark: test fore dices

---
+++ Sources/Tests/Model_UTs/Dice/ColorDieTest.cs
@@ -0,0 +1,55 @@
+﻿using Model.Dice.Faces;
+using Model.Dice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Drawing;
+
+namespace Tests.Model_UTs.Dice
+{
+    public class ColorDieTest
+    {
+        public static IEnumerable<object[]> Data_Uri()
+        {
+            yield return new object[]
+            {
+                new ColorFace(Color.FromName("Chocolate")),
+                new ColorFace(Color.FromName("Aqua")),
+                new ColorFace(Color.FromName("Beige")),
+                new ColorFace(Color.FromName("Black")),
+                new ColorFace(Color.FromName("BurlyWood")),
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data_Uri))]
+        public void RndmFaceTest(ColorFace f1, ColorFace f2, ColorFace f3, ColorFace f4, ColorFace f5)
+        {
+            //Arrange
+            List<ColorFace> listFaces = new() {
+                f1,f2,f3,f4,f5
+            };
+            ColorDie die = new(
+                listFaces[1],
+                listFaces[2],
+                listFaces[3],
+                listFaces[4]
+                );
+
+
+            //Act
+            ColorFace actual = (ColorFace)die.GetRandomFace();
+
+
+
+            //Assert
+            Assert.Contains(listFaces, face => face == actual);
+
+
+        }
+    }
+}
---
+++ Sources/Tests/Model_UTs/Dice/NumberDieTest.cs
@@ -0,0 +1,45 @@
+﻿using Model.Dice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Model.Dice.Faces;
+using System.Diagnostics;
+
+namespace Tests.Model_UTs.Dice
+{
+    public class NumberDieTest
+    {
+        [Fact]
+        public void RndmFaceTest()
+        {
+            //Arrange
+            List<NumberFace> listFaces = new() {
+                new NumberFace(1),
+                new NumberFace(2),
+                new NumberFace(3),
+                new NumberFace(4),
+                new NumberFace(5),
+            };
+            NumberDie die = new(
+                listFaces[1],
+                listFaces[2],
+                listFaces[3],
+                listFaces[4]
+                );
+
+
+            //Act
+            NumberFace actual = (NumberFace)die.GetRandomFace();
+
+
+
+            //Assert
+            Assert.Contains(listFaces, face =>face==actual);
+
+
+        }
+    }
+}
