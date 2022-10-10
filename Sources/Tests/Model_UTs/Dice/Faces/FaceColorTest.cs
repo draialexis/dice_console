f2957f06e216085b1d518944483f226933a7c25a
Ismail TAHA JANAN <ismailtahajanan2@gmail.com>
2022-10-10 10:30:18 +0200

    :white_check_mark: Faces Test implemented

--- Sources/Tests/Model_UTs/Dice/Faces/FaceColorTest.cs
+++ Sources/Tests/Model_UTs/Dice/Faces/FaceColorTest.cs
@@ -11,22 +11,60 @@ namespace Tests.Model_UTs.Dice.Faces
 {
     public class FaceColorTest
     {
-        [Fact]
-        public void ColorFaceValueTest()
+        public static IEnumerable<object[]> Data_Colors()
         {
-            Color color1 = Color.FromName("Chocolate");
-            Color color2 = Color.FromArgb(144, 255, 78, 240);
+            yield return new object[]
+            {
+                Color.FromName("Chocolate"),
+                Color.FromArgb(144, 255, 78, 240),
+            };
+            yield return new object[]
+            {
+                Color.FromName("Chocolate"),
+                Color.FromArgb(144, 255, 78, 240),
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data_Colors))]
+        public void ColorFaceValueTest(Color clrA,Color clrB)
+        {
+
 
             //Arrage 
-            ColorFace face1 = new(color1);
-            ColorFace face2 = new(color2);
+            ColorFace face1 = new(clrA);
+            ColorFace face2 = new(clrB);
 
             //Act
-            Color expected1 = color1;
+            Color expected1 = clrA;
             Color actual1 = face1.Value;
 
-            Color expected2 = color2;
+            Color expected2 = clrB;
             Color actual2 = face2.Value;
+
+            //Assert
+            Assert.Equal(expected1, actual1);
+            Assert.Equal(expected2, actual2);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data_Colors))]
+        public void ColorFaceValueToStringTest(Color clrA, Color clrB)
+        {
+
+
+            //Arrage 
+            ColorFace face1 = new(clrA);
+            ColorFace face2 = new(clrB);
+
+            //Act
+            string expected1 = clrA.ToString();
+            string actual1 = face1.StringValue;
+
+            string expected2 = clrB.ToString();
+            string actual2 = face2.StringValue;
 
             //Assert
             Assert.Equal(expected1, actual1);
---
+++ Sources/Tests/Model_UTs/Dice/Faces/FaceImageTest.cs
@@ -0,0 +1,74 @@
+﻿using Model.Dice.Faces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Model_UTs.Dice.Faces
+{
+    public class FaceImageTest
+    {
+        public static IEnumerable<object[]> Data_Colors()
+        {
+            yield return new object[]
+            {
+                new Uri("http://www.contoso.com/"),
+                new Uri("https://www.pedagojeux.fr/wp-content/uploads/2019/11/1280x720_LoL.jpg"),
+            };
+            yield return new object[]
+            {
+                new Uri("https://www.lacremedugaming.fr/wp-content/uploads/creme-gaming/2022/02/media-13411.jpg"),
+                new Uri("https://i1.moyens.net/io/images/2022/01/1642321015_Mises-a-jour-de-VALORANT-en-melee-a-venir-dans.jpg"),
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data_Colors))]
+        public void ImageFaceValueTest(Uri uriA, Uri uriB)
+        {
+
+
+            //Arrage 
+            ImageFace face1 = new(uriA);
+            ImageFace face2 = new(uriB);
+
+            //Act
+            Uri expected1 = uriA;
+            Uri actual1 = face1.Value;
+
+            Uri expected2 = uriB;
+            Uri actual2 = face2.Value;
+
+            //Assert
+            Assert.Equal(expected1, actual1);
+            Assert.Equal(expected2, actual2);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(Data_Colors))]
+        public void ImageFaceValueToStringTest(Uri uriA, Uri uriB)
+        {
+
+
+            //Arrage 
+            ImageFace face1 = new(uriA);
+            ImageFace face2 = new(uriB);
+
+            //Act
+            string expected1 = uriA.ToString();
+            string actual1 = face1.StringValue;
+
+            string expected2 = uriB.ToString();
+            string actual2 = face2.StringValue;
+
+            //Assert
+            Assert.Equal(expected1, actual1);
+            Assert.Equal(expected2, actual2);
+        }
+    }
+}
