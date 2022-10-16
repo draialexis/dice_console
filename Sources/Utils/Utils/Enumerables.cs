5694baf4cdaa6fc9862eb3b895a8956f3ee3833b
Alexis DRAI <alexis.drai@etu.uca.fr>
2022-10-16 23:22:52 +0200

    :white_check_mark: Unit test Utils.Enumerables

--- Sources/Utils/Utils/Enumerables.cs
+++ Sources/Utils/Utils/Enumerables.cs
@@ -3,11 +3,18 @@
     public static class Enumerables
     {
 
-        private static Dictionary<K, V> GetDictFromLists<K, V>(List<K> keys, List<V> values) => keys.Zip(values, (k, v) => new { k, v }).ToDictionary(x => x.k, x => x.v);
+        public static Dictionary<K, V> GetDictFromLists<K, V>(List<K> keys, List<V> values)
+        {
+            return keys.Zip(values, (k, v) => new { k, v })
+                .ToDictionary(x => x.k, x => x.v);
+        }
 
         public static Dictionary<K, V> FeedListsToDict<K, V>(Dictionary<K, V> kvps, List<K> keys, List<V> values)
         {
-            foreach (var kv in GetDictFromLists(keys, values)) { kvps.Add(kv.Key, kv.Value); }
+            foreach (var kv in GetDictFromLists(keys, values)) 
+            {
+                kvps.Add(kv.Key, kv.Value); 
+            }
             return kvps;
         }
     }
